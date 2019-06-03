554da42c5cd4d2849b998fe36167fa2d667840fa
H5-ZhangXian <961614107@qq.com>
2019-06-03 16:06:04 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+    	<router-view></router-view>
+		<tabBar></tabBar>
     </div>
-    <router-view/>
-  </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<script>
+	import tabBar from "@/components/tabBar/"
+	export default{
+		components:{
+			tabBar
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	#app{
+		display:flex;
+		flex-direction: column;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./modules/rem.js"
+import "@/stylesheets/main.scss"
+import "swiper/dist/css/swiper.min.css"
+import axios from "axios"
 
+Vue.prototype.$http = axios; 
 Vue.config.productionTip = false
 
 new Vue({
